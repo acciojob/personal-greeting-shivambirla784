fe5a581a645e90f4d5a9de5b151e02c36128734e
shivambirla784 <86475649+shivambirla784@users.noreply.github.com>
2024-10-28 19:21:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name, setName] = useState("");
+
+  const handleInputChange = (e) => {
+    setName(e.target.value);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <label>Enter your name:</label>
+      <input 
+        type="text" 
+        value={name} 
+        onChange={handleInputChange} 
+        placeholder="Enter your name" 
+      />
+      {name && <h2>Hello {name}!</h2>}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
